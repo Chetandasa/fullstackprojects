4dd364cc1359af5c67815c67c00b800bec91c70b
Your Name <chetandasa2814@gmail.com>
2023-03-31 21:21:48 +0530

    Full stack

--- Memories_project/server/index.js
+++ Memories_project/server/index.js
@@ -3,17 +3,22 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/post';
+
 const app = express();
+
+
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true}));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true}));
 app.use(cors());
 
-const CONNECTION_URL = "mongodb+srv://chetandasa2814:RcY5OtAUZgHcqozp@cluster0.qo33est.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_URL = 'mongodb+srv://firstfulstackmern:243AnkV7B3FsiUiU@cluster0.qo33est.mongodb.net/?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true }) 
-  .then (() => app.listen(PORT, console.log('server running on port: ${PORT}')))
-  .catch ((error) => console.log(error.message) );
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }) 
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
 
 mongoose.set('useFindAndModify', false);
---
+++ Memories_project/server/routes/post.js
@@ -0,0 +1,9 @@
+import  express, { Router }  from "express";
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('THIS WORKS!');
+});
+
+ export default router;
