fff85628359e03735beaa81e8d7a6e38abacff82
Your Name <chetandasa2814@gmail.com>
2023-04-05 17:14:32 +0530

    full satck project

--- Memories_project/server/controllers/posts.js
+++ Memories_project/server/controllers/posts.js
@@ -1,3 +1,4 @@
+import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
 export const getPosts = async (req, res) => {
@@ -24,4 +25,26 @@ export const createPost = async (req, res) => {
   } catch (error) {
     res.status(409).json({ message: error.message });
   }
+}
+
+export const updatePost = async (req,res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+
+  if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatePost = await PostMessage.findByIdAndUpdate(_id, post, { new: true });
+
+  res.json(updatePost);
+}
+
+export const deletePost = async (req, res) => {
+  const { id } = req.params;
+  if(!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send('No post with that id');
+
+  await PostMessage.findByIdAndRemove(id);
+
+  console.log('DELETE!');
+
+  res.json({ message: 'Post deleted successfully'});
 }
