a645c60d407ea021f5269c5872e323e088b57846
Your Name <chetandasa2814@gmail.com>
2023-04-08 00:02:04 +0530

    Full stack project

---
+++ Memories_project/client/src/components/Navbar/Navbar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AppBar, Avatar, Button, Toolbar, Typography } from '@material-ui/core';
+
+import useStyles from './styles';
+import memories from "../../images/memories.png";
+
+
+const Navbar = () => {
+  const classes = useStyles();
+
+  const user = null;
+
+  return (
+    <AppBar className={classes.appBar} position="static" color="inherit">
+      <div className={classes.brandContainer}>
+        <Typography component={Link} to="/"  className={classes.heading} variant="h2" align="center">Memories</Typography>
+        <img className={classes.image} src={memories} alt="memories" height="60" />
+        <Toolbar className={classes.toolbar}>
+          {user ? (
+            <div className={classes.profile}>
+              <Avatar className={classes.purple} alt={user.result.name} src={user.result.imageUrl}>{user.result.name.charAt(0)}</Avatar>
+              <Typography className={classes.userName} variant="h6">{user.result.name}</Typography>
+              <Button variant='container' className={classes.logoout} color="secondary" > Logout</Button>
+            </div>
+          ) : (
+            <Button component={Link} to="/auth" variant='contained' color='primary'>Sign In</Button>
+          )}
+        </Toolbar>
+      </div>
+    </AppBar>
+  );
+};
+
+export default Navbar
